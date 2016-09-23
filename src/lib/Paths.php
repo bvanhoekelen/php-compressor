8e90022f2ed2a6e5178e7e5cbe37e396ee475dc9
Bart van Hoekelen <bart@stucomm.com>
2016-09-23 15:57:14 +0200

    Add lib paths

---
+++ src/lib/Paths.php
@@ -0,0 +1,47 @@
+<?php
+
+class Paths {
+
+    private $paths;
+
+    public function __construct()
+    {
+        $this->paths = [];
+    }
+
+    /*
+     * Set the path to CSS and JS
+     */
+    public function setMultiple(Array $paths)
+    {
+        foreach ($paths as $path)
+        {
+            $this->set($path);
+        }
+    }
+
+    /*
+     * Set path
+     */
+    public function set($path)
+    {
+        if( ! is_dir($path))
+            new ErrorMessage($this,
+                'Path to "' . $path . '" does not exists!',
+                'Check the `paths` path!',
+                ['path' => $path]);
+
+        if( ! isset($this->paths[$path]))
+            $this->paths[] = $path;
+
+    }
+
+    /*
+     * Return paths
+     */
+    public function get()
+    {
+        return $this->paths;
+    }
+
+}
