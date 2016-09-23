ad6597725a27de237eee4a1c5370a1fdd130deae
Bart van Hoekelen <bart@stucomm.com>
2016-09-23 15:57:26 +0200

    Add lib compressor type

---
+++ src/lib/CompressorType.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: bartvanhoekelen
+ * Date: 23-09-16
+ * Time: 00:38
+ */
+class CompressorType {
+
+    const COMPRESSOR_TYPE_FULL = 'COMPRESSOR_TYPE_FULL';
+
+    private $type;
+
+    public function __construct()
+    {
+        $this->type = self::COMPRESSOR_TYPE_FULL;
+    }
+
+    /*
+     * Set destination
+     */
+    public function set($compressorType)
+    {
+
+        $refl = new ReflectionClass(__CLASS__);
+        $types = $refl->getConstants();
+
+        if( ! isset($types[$compressorType]))
+            new ErrorMessage($this,
+                'Compressor type "' . $compressorType . '" does not exist',
+                'Check `compressor type`!',
+                [
+                    'Your compressor type' => $compressorType,
+                    'Accept compressor types' => $types
+                ]);
+
+        $this->type = $compressorType;
+    }
+
+    /*
+     * Return destination
+     */
+    public function get()
+    {
+        return $this->type;
+    }
+
+}
